a65b743285a7397005bc582c1d9b724c7d703239
David McLaren <damlaren@gmail.com>
2015-01-31 09:14:42 -0800

    adding work on unity multiplayer test

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string typeName = "UniqueGameName";
+	private const string gameName = "RoomName";
+
+	// List of hosts retrieved from master server.
+	private HostData[] hostList;
+
+	// All clients within game will see this object
+	public GameObject playerPrefab;
+
+	// Not a callback-- start server on command only
+	private void StartServer()
+	{
+		Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+
+	// Interface for connecting to a server
+	void OnGUI()
+	{
+		// TODO: what if master server isn't local server?
+		
+		// GUI only shows if not already a server or a client
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				StartServer();
+			
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+			{
+				RefreshHostList();
+				print (hostList);
+			}
+			
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
+				}
+			}
+		}
+	}
+
+	// Called when server is initialized
+	void OnServerInitialized()
+	{
+		SpawnPlayer ();
+	}
+
+	// Get host list from master server
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+
+	// Once host list is received, OnMasterServerEvent is triggered.
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+
+	// Join server specified by host data
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+
+	private void SpawnPlayer()
+	{
+		// Instantiate a prefab on all clients, with a specific position and orientation.
+		Network.Instantiate(playerPrefab, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+	}
+
+	// Invoked once connected to server
+	void OnConnectedToServer()
+	{
+		SpawnPlayer ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		// Set local machine as master server.
+		// Default master server is run by Unity and could be down.
+		// TODO: it is possible to build and run the Unity master server on your own machine.
+		// It is desirable to run it locally in case Unity's master goes down.
+		// 
+		//MasterServer.ipAddress = "127.0.0.1";
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public float speed = 10f;
+	
+	void Update()
+	{
+		if (networkView.isMine)
+		{
+			InputMovement();
+			InputColorChange();
+		}
+		else
+		{
+			SyncedMovement();
+		}
+	}
+	
+	private void InputColorChange()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+			ChangeColorTo(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+	}
+
+	// Note this function is marked as an RPC.
+	[RPC] void ChangeColorTo(Vector3 color)
+	{
+		renderer.material.color = new Color(color.x, color.y, color.z, 1f);
+		
+		if (networkView.isMine) {
+			// RPC is sent to all "other" clients, not this one
+			networkView.RPC ("ChangeColorTo", RPCMode.OthersBuffered, color);
+		}
+	}
+	
+	private void SyncedMovement()
+	{
+		// TODO: not sure how they arrived at this calculation?
+		syncTime += Time.deltaTime;
+		rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+	}
+	
+	void InputMovement()
+	{
+		if (Input.GetKey(KeyCode.W))
+			rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.S))
+			rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.D))
+			rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.A))
+			rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+	}
+	
+	private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 syncStartPosition = Vector3.zero;
+	private Vector3 syncEndPosition = Vector3.zero;
+
+	// This function is called automatically when it sends or recieves data.
+	// Player script component must first be dragged into the Network View's "observed" field
+	// before this function gets called.
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		Vector3 syncPosition = Vector3.zero;
+		Vector3 syncVelocity = Vector3.zero;
+		if (stream.isWriting)
+		{
+			syncPosition = rigidbody.position;
+			stream.Serialize(ref syncPosition);
+			
+			syncVelocity = rigidbody.velocity;
+			stream.Serialize(ref syncVelocity);
+		}
+		else
+		{
+			// reading movements by other players
+			stream.Serialize(ref syncPosition);
+			stream.Serialize(ref syncVelocity);
+
+			// TODO: Interpolation is wonky at the beginning: if you start the server and move the cube,
+			// then start a client, you'll see in the client that the cube slowly slides over to its
+			// current position instead of starting there.
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+			
+			syncEndPosition = syncPosition + syncVelocity * syncDelay;
+			syncStartPosition = rigidbody.position;
+		}
+	}
+}
